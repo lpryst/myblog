524937da43d73e7a65012c98cb91c449c807b629
“ishtar23” <lilipryst@gmail.com>
2017-04-11 21:15:58 +0300

    Security, edit, publish, drafts, remove post

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 import blog
 from blog import urls
 
+from django.contrib.auth import views
+
 urlpatterns = [
 	url(r'^admin/', include(admin.site.urls)),
+	url(r'^accounts/login/$', views.login, name='login'),
+	url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 	url(r'', include(blog.urls)),
 ]
